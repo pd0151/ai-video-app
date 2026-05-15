79402cdde32ae3f9e5b3b0115ab792a1a6dc84ef
pd0151 <peterdillon809@gmail.com>
2026-05-15 06:30:54 +0100

    feed video fit

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -543,7 +543,7 @@ boxShadow: "0 0 44px rgba(34,255,127,0.08)",
 const media: React.CSSProperties = {
 width: "100%",
 height: "100%",
-objectFit: "contain",
+objectFit: "cover",
 objectPosition: "center",
 background: "#020617",
 display: "block",
