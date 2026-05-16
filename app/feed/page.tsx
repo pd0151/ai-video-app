00198d5dbebcf4e25ff1aafca0ac53febc85253d
pd0151 <peterdillon809@gmail.com>
2026-05-16 19:17:42 +0100

    ajust feed page

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -520,7 +520,7 @@ position: "relative",
 display: "flex",
 alignItems: "center",
 justifyContent: "center",
-padding: "190x 22px 170px",
+padding: "160x 12px 116px",
 boxSizing: "border-box",
 };
 
