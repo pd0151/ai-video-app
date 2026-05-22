3fb2c059e451b76774dde89482d33e3d0e0dee4f
pd0151 <peterdillon809@gmail.com>
2026-05-22 09:06:10 +0100

    add reset password route

---
+++ app/reset-password/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function ResetPasswordPage() {
+return (
+<main
+style={{
+minHeight: "100vh",
+background: "#04140c",
+color: "white",
+display: "flex",
+justifyContent: "center",
+alignItems: "center",
+fontSize: 24,
+}}
+>
+Reset Password Page
+</main>
+);
+}
