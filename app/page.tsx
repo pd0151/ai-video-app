b840cb957496010606a4c6ce525ae6918c37ac47
pd0151 <peterdillon809@gmail.com>
2026-05-24 19:59:03 +0100

    working homepage premo videos

--- app/page.tsx
+++ app/page.tsx
@@ -793,8 +793,8 @@ width: "max-content",
 >
 {[
 "https://videos.pexels.com/video-files/855450/855450-hd_1080_1920_30fps.mp4",
-"https://videos.pexels.com/video-files/2795405/2795405-uhd_1440_2560_25fps.mp4",
-"https://videos.pexels.com/video-files/3195394/3195394-uhd_1440_2560_25fps.mp4",
+"https://videos.pexels.com/video-files/855450/855450-hd_1080_1920_30fps.mp4",
+"https://videos.pexels.com/video-files/855450/855450-hd_1080_1920_30fps.mp4",
 "https://videos.pexels.com/video-files/855450/855450-hd_1080_1920_30fps.mp4",
 ].map((src, i) => (
 <div key={i} style={{ ...premiumAdCard, flex: "0 0 210px" }}>
