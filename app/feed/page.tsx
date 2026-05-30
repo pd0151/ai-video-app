3243ad2aa25d421319807a2de325f1df039e9acb
pd0151 <peterdillon809@gmail.com>
2026-05-30 21:13:46 +0100

    fix picture

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -496,13 +496,16 @@ bottom: 0,
 const imageMedia: CSSProperties = {
 width: "100%",
 height: "100%",
-objectFit: "cover",
+objectFit: "contain",
+background: "black",
 display: "block",
 };
+
 const media: CSSProperties = {
 width: "100%",
 height: "100%",
-objectFit: "cover",
+objectFit: "contain",
+background: "black",
 display: "block",
 };
 
