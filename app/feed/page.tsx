52d66b1ea059db417683243ffbf52e8e967cf16c
pd0151 <peterdillon809@gmail.com>
2026-05-31 20:30:43 +0100

    fix image

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -505,11 +505,10 @@ justifyContent: "center",
 
 
 const media: CSSProperties = {
-position: "absolute",
 inset: 0,
 width: "100%",
 height: "100%",
-objectFit: "contain",
+objectFit: "fill",
 objectPosition: "50% 50%",
 };
 
