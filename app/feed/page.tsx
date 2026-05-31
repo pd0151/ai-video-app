f45241facb79543c53c3b5aba8ccd007e6fa5674
pd0151 <peterdillon809@gmail.com>
2026-05-31 10:12:35 +0100

    crop image

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -497,7 +497,7 @@ const imageMedia: CSSProperties = {
 width: "100%",
 height: "100%",
 objectFit: "fill",
-objectPosition: " center",
+objectPosition: " center center",
 display: "block",
 };
 const media: CSSProperties = {
