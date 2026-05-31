6efd1a2e460c1da5f03faa995ee5d43636116a66
pd0151 <peterdillon809@gmail.com>
2026-05-31 12:34:08 +0100

    fix image size

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -504,7 +504,7 @@ justifyContent: "center",
 const uploadedImageMedia: CSSProperties = {
 width: "100%",
 height: "100%",
-objectFit: "fill",
+objectFit: "contain",
 objectPosition: "center center",
 display: "block",
 };
