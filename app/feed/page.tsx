6884c6c220ec7a27d88857011500969a19647f16
pd0151 <peterdillon809@gmail.com>
2026-05-31 20:27:23 +0100

    fix image

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -493,8 +493,8 @@ left: 0,
 right: 0,
 bottom: 0,
 display: "flex",
-alignItems:  "flex",
-justifyContent: "flex",
+alignItems:  "center",
+justifyContent: "center",
 };
 
 
@@ -510,7 +510,7 @@ inset: 0,
 width: "100%",
 height: "100%",
 objectFit: "contain",
-objectPosition: "center",
+objectPosition: "50% 50%",
 };
 
 
