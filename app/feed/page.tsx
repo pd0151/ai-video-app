3012fe4e5a67590a6b85c85558ac24801a1e1cf6
pd0151 <peterdillon809@gmail.com>
2026-05-31 17:12:35 +0100

    fix video

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -561,7 +561,7 @@ display: "block",
 const media: CSSProperties = {
 width: "100%",
 height: "100%",
-objectFit: "cover",
+objectFit: "contain",
 objectPosition: "center center",
 display: "block",
 };
