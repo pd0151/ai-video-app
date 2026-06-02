571d20ec36423c9ecdbed6303e98b0d71cb42011
pd0151 <peterdillon809@gmail.com>
2026-06-02 19:24:16 +0100

    refresh page

--- app/page.tsx
+++ app/page.tsx
@@ -642,6 +642,18 @@ color: "#05070b",
 Share to Feed
 </button>
 
+
+
+
+<img
+src={image}
+alt="Generated advert"
+style={{
+width: "100%",
+borderRadius: 22,
+display: "block",
+}}
+/>
 <button
 onClick={() => setShowPostEverywhere(true)}
 style={{
@@ -659,16 +671,6 @@ fontSize: 16,
 Post Everywhere
 </button>
 
-
-<img
-src={image}
-alt="Generated advert"
-style={{
-width: "100%",
-borderRadius: 22,
-display: "block",
-}}
-/>
 </div>
 )}
 
