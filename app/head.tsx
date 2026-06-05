513dd1e287e51cb5a22e6297f375e00cf061e253
pd0151 <peterdillon809@gmail.com>
2026-06-05 14:49:24 +0100

    add new file

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+return (
+<>
+<title>Local Business Directory | Find Local Businesses Near You | AdForge</title>
+<meta
+name="description"
+content="Find trusted local businesses near you. Advertise your business for free, generate AI images and videos, attract more customers and grow your business with AdForge."
+/>
+</>
+);
+}
