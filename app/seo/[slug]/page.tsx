67a4db9412c244ff5d5907ec79a485b9e9db592e
pd0151 <peterdillon809@gmail.com>
2026-09-05 13:46:12 +0100

    UPDATE PAGE

--- app/seo/[slug]/page.tsx
+++ app/seo/[slug]/page.tsx
@@ -3,6 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 import { notFound } from "next/navigation";
 import Script from "next/script";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
